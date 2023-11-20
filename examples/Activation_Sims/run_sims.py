3ecc8dd18895eae0f2ebfc3933c0d5efac2c7aff
Christopher Karwin <ckarwin@login002.palmetto.clemson.edu>
2023-11-20 17:54:34 -0500

    updated activation example and batch example

--- examples/Activation_Sims/run_sims.py
+++ examples/Activation_Sims/run_sims.py
@@ -1,39 +1,32 @@
 # Imports:
 from cosi_dc.pipeline.run_dc import RunDataChallenge
 
-### INTRO ###
-# Run activation sims using run_type = array_jobs-parallel.
-# This allowed us to simulate 3 months of activation for DC2!
-
 ### SIMULATION SETUP ###
 # Define instance with input parameter card:
 instance = RunDataChallenge("inputs.yaml")
 
-# Generate source file:
-# Here we will specify that we are using an external source.
-# The source files need to be generated using the cosi-background class,
-# and copied to the Source directory (in the run directory). 
+# Generate source files:
+# The source files are available in the DC2 source library: 
+# https://github.com/cositools/cosi-data-challenges/tree/main/cosi_dc/Source_Library/DC2/backgrounds
+# They are generated using the cosi-background class,
 # For steps 1 and 3, set the exposure time to the max time in the ori/LC files. 
 # For step 2, set the irradiation time to your preference (we use 1 yr for DC2).
 # The genaric name for the LC is lightcurve.dat,
 # and the genaric name for the ori file is GalacticScan.ori. 
-#instance.define_sim(external_src=True)
+instance.define_sim(external_src=True)
 
 ### COSIMA ###
-# run cosima, steps 1-3:
-# You must run each step one at a time. 
-# This will be managed using array_jobs.sh, 
-# which utilizes a combination of job arrays (i.e. MPI) and gnu_parallel.
-# Run as many jobs in parallel as possible, based on the available resources, 
-# but do not give multiple MEGAlib commands within a single run, 
-# otherwise the system will be overloaded and jobs won't finish properly.
-# Make sure to check the output at each stage after all jobs finish, 
-# to verify that everying is running ok. 
-# The parameter get_cpu in check cosima_parallel must be set to False for step 2. 
-# Make sure to mofify the isotope sim time after step 1 if using an ori file!
+# Run cosima, steps 1-3:
+# Run as many jobs in parallel as possible, based on the available resources. 
 instance.name = "PrimaryProtons" # Name of component
 instance.run_cosima(output_name="cosima_step1")
+
+# Make sure to check the output to verify that everying is running ok. 
+# The parameter get_cpu in check cosima_parallel must be set to False for checking step 2.
 instance.check_cosima_parallel(input_file="cosima_step1_terminal_output.txt", get_cpu=True, show_plot=False)
+
+# Make sure to modify the isotope sim time after step 1 if using an ori file!
+# The parameter 'TT' in the isotope file should match the time bin size in the ori file. 
 instance.modify_isotope_sim_time("PrimaryProtonsIsotopes.inc1.dat",45.0)
 
 # If you need to modify times in the sim file in order to match the decay component with the orientation times:
@@ -60,4 +53,7 @@ instance.check_revan_parallel(input_file="revan_decay_terminal_output.txt",savef
 ### MIMREC ###
 # Run mimrec:
 instance.run_mimrec(extract_root=True)
+instance.plot_spectrum()
+instance.plot_lc()
+instance.plot_image()
 instance.clear_unessent:ial_data()
--- examples/batch_scripts/pbs/README.txt
+++ examples/batch_scripts/pbs/README.txt
@@ -1,11 +1,17 @@
 These scripts have been used on Clemson's palmetto cluster, which uses PBS. 
+The recommended scripts to use are array_job.sh and array_job-parallel.sh. 
 
 array_job.sh:
 Basic script for submitting array jobs, 
 without manually specifying multiple CPUs
 per node. In this case, a single job will run
 on a single node, using only 1 cpu. Generally,
-this is a waste of CPUs. 
+this is a waste of CPUs. However, many clusters
+have special paritions that will automatically send 
+jobs of an array to individual CPUs by default, which
+makes things very convenient. See the slurm directory 
+for some specific examples. This script can also be 
+used for such a partition, if available.    
 
 array_job-parallel.sh: 
 Basic script for submitting array jobs, 
